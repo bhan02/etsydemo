092aa990cd7e8b0d168d26bba17c39ca50c9635c
Bo Han <bo@learncycle.io>
2015-04-14 12:51:25 -0400

    Transfer funds to seller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413161857) do
+ActiveRecord::Schema.define(version: 20150414162932) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150413161857) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
